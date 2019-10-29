5812133a50a5df4694170a8d00857d67c93f63a3
Phill <philldamaskin@gmail.com>
2019-10-29 13:25:21 -0400

    add grab mechanic

---
+++ ThrowThings/Assets/Scripts/Holder.cs
@@ -0,0 +1,64 @@
+﻿using UnityEngine;
+
+public class Holder : MonoBehaviour
+{
+    [SerializeField]
+    private string pressButton = "";
+
+    private Grabbable holdingObject;
+    private PhysicsMaterial2D physicsMat;
+
+    private void Awake()
+    {
+        physicsMat = GetComponentInChildren<Collider2D>().sharedMaterial;
+    }
+
+    private void Update()
+    {
+        if (holdingObject && !Input.GetKey(pressButton))
+        {
+            Drop();
+        }
+    }
+
+    private void Drop()
+    {
+        holdingObject.transform.SetParent(null);
+        holdingObject.RestoreRigidbody();
+        holdingObject = null;
+    }
+
+    private void OnCollisionStay2D(Collision2D collision)
+    {
+        if (holdingObject)
+        {
+            return;
+        }
+
+        if (Input.GetKey(pressButton))
+        {
+            for (int i = 0; i < collision.contactCount; i++)
+            {
+                ContactPoint2D contact = collision.GetContact(i);
+                if (Grabbable.TryGet(contact.collider.transform, out Grabbable grabbable))
+                {
+                    Grab(grabbable);
+                    return;
+                }
+            }
+        }
+    }
+
+    private void Grab(Grabbable grabbable)
+    {
+        holdingObject = grabbable;
+        holdingObject.RemoveRigidbody();
+        grabbable.transform.SetParent(transform);
+
+        Collider2D[] colliders = GetComponentsInChildren<Collider2D>();
+        for (int i = 0; i < colliders.Length; i++)
+        {
+            colliders[i].sharedMaterial = physicsMat;
+        }
+    }
+}
--- ThrowThings/Assets/Scripts/Movement.cs
+++ ThrowThings/Assets/Scripts/Movement.cs
@@ -14,6 +14,9 @@ public class Movement : MonoBehaviour
     [SerializeField]
     private string horizontalInput = "Horizontal1";
 
+    [SerializeField]
+    private bool allowKeyboard = false;
+
     private Rigidbody2D rb;
 
     private void Awake()
@@ -25,9 +28,30 @@ public class Movement : MonoBehaviour
     private void FixedUpdate()
     {
         float direction = Input.GetAxisRaw(horizontalInput);
+        if (allowKeyboard)
+        {
+            if (Input.GetKey(KeyCode.A) || Input.GetKey(KeyCode.LeftArrow))
+            {
+                direction = -1f;
+            }
+            else if (Input.GetKey(KeyCode.D) || Input.GetKey(KeyCode.RightArrow))
+            {
+                direction = 1f;
+            }
+        }
+
         Vector2 velocity = rb.velocity;
         velocity.x = Mathf.Lerp(velocity.x, direction * speed, Time.fixedDeltaTime * accel);
         rb.velocity = velocity;
         rb.AddForce(gravity * Vector2.down * rb.mass);
+    }
+
+    private void OnCollisionStay2D(Collision2D collision)
+    {
+        if (collision.rigidbody)
+        {
+            //rb.MovePosition(rb.position + collision.rigidbody.velocity * Time.fixedDeltaTime);
+            rb.velocity += collision.rigidbody.velocity * 0.15f;
+        }
     }
 }
