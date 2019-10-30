18e0fcb998f2d0573866bd8a8f767d95545ef183
langloistaylorj <57120599+langloistaylorj@users.noreply.github.com>
2019-10-30 13:17:49 -0400

    Dropper now takes an array instead of individual objects
    Minor changes to scene, added objects to array in inspector
    
    A few micro-optimizations

--- ThrowThings/Assets/Scripts/Dropper.cs
+++ ThrowThings/Assets/Scripts/Dropper.cs
@@ -4,36 +4,16 @@ using UnityEngine;
 
 public class Dropper : MonoBehaviour
 {
-    new GameObject[] Instant;
     int i = -1;
 
-    new GameObject[] Possible;
+    new GameObject[] Instant = new GameObject[5];
+
 
     [SerializeField]
-    private GameObject Obj1;
-
-    [SerializeField]
-    private GameObject Obj2;
-
-    [SerializeField]
-    private GameObject Obj3;
-
-    [SerializeField]
-    private GameObject Obj4;
+    new GameObject[] Objs = new GameObject[4];
 
     float timer = 0;
     float MaxTimer = 3;
-    // Start is called before the first frame update
-    void Start()
-    {
-        Instant = new GameObject[5];
-        Possible = new GameObject[4];
-
-        Possible[0] = Obj1;
-        Possible[1] = Obj2;
-        Possible[2] = Obj3;
-        Possible[3] = Obj4;
-    }
 
     // Update is called once per frame
     void Update()
@@ -57,7 +37,7 @@ public class Dropper : MonoBehaviour
                 Destroy(Instant[i]);
             }
 
-            Instant[i] = Instantiate(Possible[Random.Range(0,3)], new Vector3(Random.Range(-5.5f, 5.5f),10,0), Quaternion.identity);
+            Instant[i] = Instantiate(Objs[Random.Range(0,3)], new Vector3(Random.Range(-5.5f, 5.5f),10,0), Quaternion.identity);
             Instant[i].transform.localScale += new Vector3(Random.Range(0.7f, 1.3f), Random.Range(0.7f, 1.3f), 1);
         }
     }
