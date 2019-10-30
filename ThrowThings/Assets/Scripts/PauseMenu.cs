5807ed647b8a080adf27bf9b2f5f0cc6df843a83
zk213 <goujisha@gmail.com>
2019-10-30 15:51:12 -0400

    Pause Menu and Start Menu

---
+++ ThrowThings/Assets/Scripts/PauseMenu.cs
@@ -0,0 +1,74 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+public class PauseMenu : MonoBehaviour
+{
+    public static bool GameIsPaused = false;
+
+    public GameObject PauseMenuUI;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+
+            if (GameIsPaused)
+            {
+                Resume();
+            }
+            else
+            {
+
+                Pause();
+            }
+        }
+    }
+
+
+    public void Resume()
+    {
+        PauseMenuUI.SetActive(false);
+        Time.timeScale = 1f;
+        GameIsPaused = false;
+
+
+    }
+    void Pause()
+    {
+
+        PauseMenuUI.SetActive(true);
+        Time.timeScale = 0f;
+        GameIsPaused = true;
+
+
+    }
+
+    public void LoadMenu()
+    {
+        Time.timeScale = 1f;
+        SceneManager.LoadScene(0);
+
+    }
+
+    public void QuitGame()
+    {
+
+        Application.Quit();
+
+    }
+
+    public void PlayGame()
+    {
+        SceneManager.LoadScene(1);
+
+
+
+    }
+}
