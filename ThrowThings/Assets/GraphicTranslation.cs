e38ebecab3fdd6b4a55a585f3c391e6e1efdef24
langloistaylorj <57120599+langloistaylorj@users.noreply.github.com>
2019-10-31 15:28:32 -0400

    gave tower bottoms sprites

---
+++ ThrowThings/Assets/GraphicTranslation.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GraphicTranslation : MonoBehaviour
+{
+    // Update is called once per frame
+    void Update()
+    {
+        
+        if (gameObject.transform.root.gameObject.GetComponent<Rigidbody2D>().velocity.x >= 0.5)
+        {
+            GetComponent<Animator>().SetBool("Moving", true);
+        }
+        GetComponent<Animator>().SetBool("Moving", false);
+    }
+}
