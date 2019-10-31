375b6e78feb2cb66f9cc4e9af9a2d4892595076a
zk213 <goujisha@gmail.com>
2019-10-31 12:03:18 -0400

    Revert "make thrown props break other props"
    This reverts commit 729ca46822706af51de2042d3b0f68a27ca54dde.

--- ThrowThings/Assets/Scripts/StickToPlatform.cs
+++ ThrowThings/Assets/Scripts/StickToPlatform.cs
@@ -2,41 +2,13 @@
 
 public class StickToPlatform : MonoBehaviour
 {
-    public int team;
-    
-    private bool stuck;
-    
     private void OnCollisionEnter2D(Collision2D collision)
     {
-        if (!stuck)
+        if (collision.collider.attachedRigidbody && collision.collider.attachedRigidbody.name == "Top")
         {
-            StickToPlatform stick = collision.collider.GetComponentInParent<StickToPlatform>();
-            if (stick)
-            {
-                if (team != stick.team)
-                {
-                    Destroy(gameObject);
-                    Destroy(stick.gameObject);
-                }
-                else
-                {
-                    transform.SetParent(stick.transform);
-                    Rigidbody2D rb = GetComponent<Rigidbody2D>();
-                    if (rb)
-                    {
-                        Destroy(rb);
-                    }
-
-                    stuck = true;
-                    Finish.IgnoreCollision(GetComponent<Collider2D>(), false);
-                }
-                
-                return;
-            }
-            
             WaveyThing thing = collision.collider.GetComponentInParent<WaveyThing>();
-            stuck = thing.Attach(gameObject);
-            if (stuck)
+            enabled = !thing.Attach(gameObject);
+            if (!enabled)
             {
                 Finish.IgnoreCollision(GetComponent<Collider2D>(), false);
             }
