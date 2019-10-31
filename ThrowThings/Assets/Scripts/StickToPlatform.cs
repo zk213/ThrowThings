7571d3f5e0c7a37218be854aab11a4d19bf095d9
Phill <philldamaskin@gmail.com>
2019-10-31 16:09:30 -0400

    Update StickToPlatform.cs

--- ThrowThings/Assets/Scripts/StickToPlatform.cs
+++ ThrowThings/Assets/Scripts/StickToPlatform.cs
@@ -8,35 +8,35 @@ public class StickToPlatform : MonoBehaviour
     
     private void OnCollisionEnter2D(Collision2D collision)
     {
-        if (!stuck)
-        {
-            StickToPlatform stick = collision.collider.GetComponentInParent<StickToPlatform>();
-            if (stick)
-            {
-                if (transform.position.y < Finish.Y)
-                {
-                    if (team != stick.team)
-                    {
-                        Destroy(gameObject);
-                        Destroy(stick.gameObject);
-                    }
-                    else
-                    {
-                        transform.SetParent(stick.transform);
-                        Rigidbody2D rb = GetComponent<Rigidbody2D>();
-                        if (rb)
-                        {
-                            Destroy(rb);
-                        }
+        //if (!stuck)
+        //{
+        //    StickToPlatform stick = collision.collider.GetComponentInParent<StickToPlatform>();
+        //    if (stick)
+        //    {
+        //        if (transform.position.y < Finish.Y)
+        //        {
+        //            if (team != stick.team)
+        //            {
+        //                Destroy(gameObject);
+        //                Destroy(stick.gameObject);
+        //            }
+        //            else
+        //            {
+        //                transform.SetParent(stick.transform);
+        //                Rigidbody2D rb = GetComponent<Rigidbody2D>();
+        //                if (rb)
+        //                {
+        //                    Destroy(rb);
+        //                }
 
-                        stuck = true;
-                        Finish.IgnoreCollision(GetComponent<Collider2D>(), false);
-                    }
+        //                stuck = true;
+        //                Finish.IgnoreCollision(GetComponent<Collider2D>(), false);
+        //            }
 
-                    return;
-                }
-            }
-        }
+        //            return;
+        //        }
+        //    }
+        //}
 
         WaveyThing thing = collision.collider.GetComponentInParent<WaveyThing>();
         if (thing)
