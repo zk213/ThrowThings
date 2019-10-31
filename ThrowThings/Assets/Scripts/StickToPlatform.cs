729ca46822706af51de2042d3b0f68a27ca54dde
Phill <philldamaskin@gmail.com>
2019-10-31 11:16:02 -0400

    make thrown props break other props

--- ThrowThings/Assets/Scripts/StickToPlatform.cs
+++ ThrowThings/Assets/Scripts/StickToPlatform.cs
@@ -2,13 +2,41 @@
 
 public class StickToPlatform : MonoBehaviour
 {
+    public int team;
+    
+    private bool stuck;
+    
     private void OnCollisionEnter2D(Collision2D collision)
     {
-        if (collision.collider.attachedRigidbody && collision.collider.attachedRigidbody.name == "Top")
+        if (!stuck)
         {
+            StickToPlatform stick = collision.collider.GetComponentInParent<StickToPlatform>();
+            if (stick)
+            {
+                if (team != stick.team)
+                {
+                    Destroy(gameObject);
+                    Destroy(stick.gameObject);
+                }
+                else
+                {
+                    transform.SetParent(stick.transform);
+                    Rigidbody2D rb = GetComponent<Rigidbody2D>();
+                    if (rb)
+                    {
+                        Destroy(rb);
+                    }
+
+                    stuck = true;
+                    Finish.IgnoreCollision(GetComponent<Collider2D>(), false);
+                }
+                
+                return;
+            }
+            
             WaveyThing thing = collision.collider.GetComponentInParent<WaveyThing>();
-            enabled = !thing.Attach(gameObject);
-            if (!enabled)
+            stuck = thing.Attach(gameObject);
+            if (stuck)
             {
                 Finish.IgnoreCollision(GetComponent<Collider2D>(), false);
             }
